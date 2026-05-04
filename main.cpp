44c890ddd63d770c7eb7ab454d7fce629db10bac
Ahsan Khan <ahsan125384@gmail.com>
2026-05-05 00:55:58 +0500

    forward propagation completed

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <ctime>
 #include <cmath>
 #include <vector>
+#include <algorithm>
+
 
 // Also write understandable comments in the code to explain what each part does.
 
@@ -39,10 +41,40 @@ int main()
                 temp += input[j] * weights[i][j]; // Calculate the weighted sum of inputs for each neuron
                 
             }
-            temp += bias[i]; // Add the bias to the weighted sum
-            output = 1/(1+exp(-temp));
-            hidden_layer[i] = output;
-        }  
+            temp += bias[i];            // Add the bias to the weighted sum
+            output = max(0.0,temp);     // Apply the ReLU activation function to the weighted sum to get the output of the neuron
+          hidden_layer[i] = output;   // Store the output of the neuron in the hidden layer vector    
+        }
+
+     vector<vector<double>> outer_weights(10, vector<double>(128));
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 128; j++)
+            {
+                double randomWeight2 = ((double)rand() / RAND_MAX) - 0.5;
+                outer_weights[i][j] = randomWeight2;
+            }
+        }
+   
+    vector<double>output_layer(10);
+    for(int i=0;i<10;i++){
+        double output;
+        double temp = 0.0;
+        for(int j=0;j<128;j++){
+            temp += hidden_layer[j] *outer_weights[i][j];
+
+        }
+        temp += bias[i];
+        double sum =0;
+        
+        output_layer[i]= temp;
+        for(int k=0;k<10;k++){
+            sum += exp(output_layer[k]);
+         }
+        output_layer[i] = exp(output_layer[i])/sum; // Apply the softmax activation function to the output layer to get the final output probabilities for each class
+        
+    }
+
+    
     
     return 0;
 }
